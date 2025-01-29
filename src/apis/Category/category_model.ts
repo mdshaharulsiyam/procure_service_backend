d22fe455a64f589d5f50dee630bebdf1d3fd081b
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-01-28 23:59:52 -0600

    [category] category model created

---
+++ src/apis/Category/category_model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+import { ICategory } from "./category_type";
+
+const category_schema = new Schema<ICategory>({
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    img: {
+        type: String,
+        required: [true, 'img is required']
+    },
+    available_position: {
+        type: Number,
+        default: 0
+    },
+    total_service: {
+        type: Number,
+        default: 0
+    },
+    quote_price: {
+        type: Number,
+        default: 1
+    }
+})
+
+export const category_model = model<ICategory>('category', category_schema);
---
+++ src/apis/Category/category_type.ts
@@ -0,0 +1,7 @@
+export interface ICategory {
+    name: string;
+    img: string;
+    available_position: Number;
+    total_service: Number;
+    quote_price: Number;
+}
