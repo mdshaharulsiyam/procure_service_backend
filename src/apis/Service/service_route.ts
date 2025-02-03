38747817cb1a8a093167da2dda33108ac8e2d89a
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-02-02 22:52:13 -0600

    [service] service issus fix

--- src/apis/Service/service_route.ts
+++ src/apis/Service/service_route.ts
@@ -3,14 +3,15 @@ import asyncWrapper from '../../middleware/asyncWrapper';
 import verifyToken from '../../middleware/verifyToken';
 import config from '../../DefaultConfig/config';
 import { category_controller } from './service_controller';
+import uploadFile from '../../middleware/fileUploader';
 
 export const service_router = express.Router()
 
 service_router
-    .post('/service/create', verifyToken(config.ADMIN), asyncWrapper(category_controller.create))
+    .post('/service/create', verifyToken(config.ADMIN), uploadFile(), asyncWrapper(category_controller.create))
 
     .get('/service/get-all', asyncWrapper(category_controller.get_all))
 
-    .patch('/service/update/:id', verifyToken(config.ADMIN), asyncWrapper(category_controller.update))
+    .patch('/service/update/:id', verifyToken(config.ADMIN), uploadFile(), asyncWrapper(category_controller.update))
 
     .delete('/service/delete/:id', verifyToken(config.ADMIN), asyncWrapper(category_controller.delete_service))
