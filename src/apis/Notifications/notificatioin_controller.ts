4a18b4c27821afff23efb75c822c0df4901cfbf2
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-02-04 05:21:37 -0600

    [notification] notification model service controller route created

---
+++ src/apis/Notifications/notificatioin_controller.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { SearchKeys } from "../../utils/Queries";
+import { HttpStatus } from "../../DefaultConfig/config";
+import { sendResponse } from "../../utils/sendResponse";
+import { notification_service } from "./notification_service";
+import { IAuth } from "../Auth/auth_types";
+
+async function get_all(req: Request, res: Response) {
+
+    const { search, ...otherValues } = req?.query;
+    const searchKeys: SearchKeys = {}
+
+    if (search) searchKeys.name = search as string
+
+    const queryKeys = {
+        ...otherValues
+    }
+
+    if (req?.user?.role != "ADMIN" && req?.user?.role != "SUPER_ADMIN") {
+        queryKeys.user = req?.user?._id as string
+    }
+
+    const populatePath: string | string[] = 'user'
+    const selectFields: string | string[] = 'name img _id'
+    const modelSelect: string = ''
+
+    const result = await notification_service.get_all(queryKeys, searchKeys, populatePath, selectFields, modelSelect)
+    sendResponse(
+        res,
+        HttpStatus.SUCCESS,
+        result
+    )
+}
+
+async function read(req: Request, res: Response,) {
+
+    const result = await notification_service.read_notification(req?.params?.id, req?.user as IAuth)
+
+    sendResponse(
+        res,
+        HttpStatus.SUCCESS,
+        result
+    )
+}
+
+export const notification_controller = Object.freeze({
+    get_all, read
+})
---
+++ src/apis/Notifications/notification_service.ts
@@ -0,0 +1,31 @@
+import Queries, { QueryKeys, SearchKeys } from "../../utils/Queries";
+import { IAuth } from "../Auth/auth_types";
+import { notification_model } from "./notification_model";
+import { INotification } from "./notification_types";
+
+async function create(data: INotification) {
+    await notification_model.insertMany(data)
+    return {
+        success: true,
+        message: 'notification created successfully',
+    }
+}
+async function get_all(queryKeys: QueryKeys, searchKeys: SearchKeys, populatePath?: any, selectFields?: string | string[], modelSelect?: string) {
+    return await Queries(notification_model, queryKeys, searchKeys, populatePath, selectFields, modelSelect)
+}
+async function read_notification(id: string, user: IAuth) {
+    const result = await notification_model.findOneAndUpdate({ _id: id, user }, {
+        $set: {
+            ...(user?.role != "ADMIN" && user?.role != "SUPER_ADMIN") ? { read_by_user: true } : { read_by_admin: true }
+        }
+    })
+    return {
+        success: true,
+        message: 'notification deleted successfully',
+        data: result
+    }
+}
+
+export const notification_service = Object.freeze({
+    create, get_all, read_notification
+})
